f24662a7e2dc81066f89a1c73331d9e46d9a2d1a
Himanshu <himanshu_be@yahoo.com>
2025-11-05 18:23:05 +0530

    Added for loop

--- myfirstapp/src/app/app.ts
+++ myfirstapp/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('myfirstapp');
+  display:boolean=true;
+  fullstack=[
+    {name:'Angular',type:'Frontend'},
+    {name:'NodeJS',type:'Backend'},
+    {name:'MongoDB',type:'Database'}
+  ]; 
+  
+  items : string []=['apple','bababa'];
 }
