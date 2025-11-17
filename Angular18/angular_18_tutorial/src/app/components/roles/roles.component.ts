17245c116fa643b69977f31b4b03cc5e2eb6633a
Himanshu <himanshu_be@yahoo.com>
2025-11-17 20:06:33 +0530

    Added event and binding data

--- Angular18/angular_18_tutorial/src/app/components/roles/roles.component.ts
+++ Angular18/angular_18_tutorial/src/app/components/roles/roles.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+  fisrtname:string = "Angular tutoial";
+  angularversion = "18";
+  version : number =19;
+  isactive: boolean = false;
+  currentdate:Date = new Date();
+  inputtype:string = "radio";
+  selectedstate : string = "";
+  
+  showWelcomeMessage() {
+    alert("woleome to angular " + this.angularversion + " tutorial");
+
+  }
+  showmessage(message:string){
+
+     alert("woleome to angular " + message + " tutorial");
+
+  }
 
 }
