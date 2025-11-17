138d255f819b5e6d5af9be7175eb648e484e9235
Himanshu <himanshu_be@yahoo.com>
2025-11-17 21:58:19 +0530

    Added role component

--- Angular18/angular_18_tutorial/src/app/components/master/master.component.ts
+++ Angular18/angular_18_tutorial/src/app/components/master/master.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DesignationComponent } from '../designation/designation.component';
+import { RolesComponent } from '../roles/roles.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [DesignationComponent,RolesComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })
 export class MasterComponent {
-
+  currentComponent:string = "";
 }
