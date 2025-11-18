87f243ac8703d8fb51431b90334974e90d47f7d5
Himanshu <himanshu_be@yahoo.com>
2025-11-18 21:39:18 +0530

    Commented non required data for components

--- Angular18/angular_18_tutorial/src/app/components/master/master.component.ts
+++ Angular18/angular_18_tutorial/src/app/components/master/master.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { DesignationComponent } from '../designation/designation.component';
 import { RolesComponent } from '../roles/roles.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [DesignationComponent,RolesComponent],
+  imports: [DesignationComponent, RolesComponent, CommonModule],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })
 export class MasterComponent {
-  currentComponent:string = "";
+  currentComponent:string = "Roles";
+
+  changetab(tabnamme:string){
+    this.currentComponent = tabnamme;
+  }
 }
--- Angular18/angular_18_tutorial/src/app/components/roles/roles.component.ts
+++ Angular18/angular_18_tutorial/src/app/components/roles/roles.component.ts
@@ -9,22 +9,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-  fisrtname:string = "Angular tutoial";
-  angularversion = "18";
-  version : number =19;
-  isactive: boolean = false;
-  currentdate:Date = new Date();
-  inputtype:string = "radio";
-  selectedstate : string = "";
+  // fisrtname:string = "Angular tutoial";
+  // angularversion = "18";
+  // version : number =19;
+  // isactive: boolean = false;
+  // currentdate:Date = new Date();
+  // inputtype:string = "radio";
+  // selectedstate : string = "";
   
-  showWelcomeMessage() {
-    alert("woleome to angular " + this.angularversion + " tutorial");
+  // showWelcomeMessage() {
+  //   alert("woleome to angular " + this.angularversion + " tutorial");
 
-  }
-  showmessage(message:string){
+  // }
+  // showmessage(message:string){
 
-     alert("woleome to angular " + message + " tutorial");
+  //    alert("woleome to angular " + message + " tutorial");
 
-  }
+  // }
 
 }
