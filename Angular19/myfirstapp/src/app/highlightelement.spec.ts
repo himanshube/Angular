fd8341af640da5a99c952438e9020df5cda7f001
Himanshu <himanshu_be@yahoo.com>
2025-12-17 21:46:27 +0530

    Added Angular 19

--- Angular19/myfirstapp/src/app/highlightelement.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Highlightelement } from './highlightelement';
-
-describe('Highlightelement', () => {
-  it('should create an instance', () => {
-    const directive = new Highlightelement();
-    expect(directive).toBeTruthy();
-  });
-});
