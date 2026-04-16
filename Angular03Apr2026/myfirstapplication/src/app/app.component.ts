b615629139b94529e3ad9506beb8367db9c67b92
Himanshu <himanshu_be@yahoo.com>
2026-04-16 22:00:25 +0530

    added

--- Angular03Apr2026/myfirstapplication/src/app/app.component.ts
+++ Angular03Apr2026/myfirstapplication/src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, TestComponent, FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+display:boolean=true;
+quantity:number =1;
+itemperquantuyy :number =100;
+
   title = 'myfirstapplication';
   msg="India is best";
   counter:number =0;
+  staticstring :string ="yjid sdiosa ";
 
    increment(str :string)
   {
@@ -22,5 +30,9 @@ export class AppComponent {
   decrement()
   {
     this.counter--;
+  }
+  onkeypress(event:any)
+  {
+    console.log(event.target.value)
   }
 }
