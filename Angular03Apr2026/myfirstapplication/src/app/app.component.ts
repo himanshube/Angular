469650b7aa076055d7b2ce03a9f821e8c23401ae
Himanshu <himanshu_be@yahoo.com>
2026-05-06 21:53:51 +0530

    Added

--- Angular03Apr2026/myfirstapplication/src/app/app.component.ts
+++ Angular03Apr2026/myfirstapplication/src/app/app.component.ts
@@ -7,30 +7,36 @@ import { EmployeeService } from './dependencies/employee.service';
 import { LogMessage1Service } from './services/log-message1.service';
 import { LogMessage2Service } from './services/log-message2.service';
 import {NgForm} from '@angular/forms';
+import { ReactiveFormsModule, FormControl,Validators,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [  FormsModule, CommonModule],
+  imports: [  FormsModule, CommonModule,ReactiveFormsModule],
   providers: [{provide:LogMessage1Service,useClass:LogMessage1Service},
     {provide:LogMessage1Service,useClass:LogMessage2Service}],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  myForm : FormGroup;
 
-
-
-submitform(myform:NgForm){
-  if(myform.valid){
-
-    const formvalue = JSON.stringify(myform.value);
-    console.log(formvalue);
-  }
-}
-
-  resetform(myform:NgForm){
-  myform.reset();
+   constructor() {
+    this.myForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      age: new FormControl('', Validators.min(18)),
+    });
   }
 
+   submitForm() {
+    const userAge = this.myForm.get('age')?.value;
+    if (userAge < 18) {
+      alert('Age must be 18 or older');
+      return;
+    } else if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    }
+  }
+   
 }
---
+++ Angular03Apr2026/myfirstapplication/src/app/rform/rform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rform',
+  standalone: true,
+  imports: [],
+  templateUrl: './rform.component.html',
+  styleUrl: './rform.component.css'
+})
+export class RformComponent {
+
+}
