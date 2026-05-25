008b13fa0948ee4f3bcdcdf8937b59f0b5a3724b
Himanshu <himanshu_be@yahoo.com>
2026-05-25 19:47:07 +0530

    Added

--- Angular03Apr2026/estore/src/app/app.routes.ts
+++ Angular03Apr2026/estore/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from "./components/home/home.component";
+import { NotFoundComponent } from './components/not-found/not-found.component';
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
---
+++ Angular03Apr2026/estore/src/app/components/sidenavigation/sidenavigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenavigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidenavigation.component.html',
+  styleUrl: './sidenavigation.component.css'
+})
+export class SidenavigationComponent {
+
+}
