ffc001f803e89e239bd9559cf71914196ad8ccf2
Himanshu <himanshu_be@yahoo.com>
2026-05-25 19:32:24 +0530

    Added links

--- Angular03Apr2026/estore/src/app/app.component.ts
+++ Angular03Apr2026/estore/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { CatnavigationComponent } from "./components/catnavigation/catnavigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CatnavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Angular03Apr2026/estore/src/app/components/catnavigation/catnavigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catnavigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './catnavigation.component.html',
+  styleUrl: './catnavigation.component.css'
+})
+export class CatnavigationComponent {
+
+}
