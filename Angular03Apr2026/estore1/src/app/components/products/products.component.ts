2b4b7178212df7e3a89f2a773dcdbd1633b2e317
Himanshu <himanshu_be@yahoo.com>
2026-05-26 14:21:17 +0530

    Added new images

--- Angular03Apr2026/estore1/src/app/components/products/products.component.ts
+++ Angular03Apr2026/estore1/src/app/components/products/products.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ProductsService } from './products.service';
+import { ProductListItem } from './products.type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
+  providers: [ProductsService]
 })
 export class ProductsComponent {
+
+  products: ProductListItem[] = [];
+
+  constructor(productsService: ProductsService) {
+    this.products = productsService.getProductsList();
+  }
 
 }
