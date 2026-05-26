6f3d82c9956ca654012e58d8d5899ffaf99f5c82
Himanshu <himanshu_be@yahoo.com>
2026-05-26 12:53:33 +0530

    Added categories

--- Angular03Apr2026/estore1/src/app/components/sidenavigation/sidenavigation.component.ts
+++ Angular03Apr2026/estore1/src/app/components/sidenavigation/sidenavigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Category } from '../home/types/category';
+import { CategoryService } from '../home/services/category.service';
 
 @Component({
   selector: 'app-sidenavigation',
@@ -8,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './sidenavigation.component.css'
 })
 export class SidenavigationComponent {
+categories : Category[]=[];
+
+constructor(categoryservice:CategoryService){
+  this.categories=categoryservice.getAllCategories();
+}
+
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
 
 }
