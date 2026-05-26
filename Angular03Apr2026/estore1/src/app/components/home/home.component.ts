1c29b170393da8e30ca5ccb516a2d19e36814ff9
Himanshu <himanshu_be@yahoo.com>
2026-05-26 13:07:08 +0530

    Added images in product

--- Angular03Apr2026/estore1/src/app/components/home/home.component.ts
+++ Angular03Apr2026/estore1/src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CatnavigationComponent } from './catnavigation/catnavigation.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenavigationComponent } from "../sidenavigation/sidenavigation.component";
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CatnavigationComponent, HeaderComponent, SidenavigationComponent],
+  imports: [CatnavigationComponent, HeaderComponent, SidenavigationComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
